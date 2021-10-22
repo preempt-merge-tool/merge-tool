10412dee7053e435e72be2d7c95db44d4fda9a38
preempt-merge-tool <92991232+preempt-merge-tool@users.noreply.github.com>
2021-10-22 09:59:08 -0400

    Remove identifying information & update step 2

--- master_merge.sh
+++ master_merge.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH -c 1                    # Request one core
-#SBATCH -N 1                    # Request one node
+#SBATCH -c 1                                  # Request one core
+#SBATCH -N 1                                  # Request one node
 
-#SBATCH -t 0-12:00              # Run time D-HH:MM format
-#SBATCH -p short                # Partition to run in
-#SBATCH --mem=2000              # Memory total in MB (in all cores)
-#SBATCH -e /home/sks59/merge-tool/clinvar-master/error/hostname_%j.err  # File to which STDERR will be written, including jobID      # Change/Delete this
-#SBATCH --mail-type=ALL         # Type of email notification- BEGIN, END, FAIL, ALL
-#SBATCH --mail-user=skstein2@gmail.com  # Email where notifications send to                              # Change/Delete this
+#SBATCH -t 0-12:00                            # Run time D-HH:MM format
+#SBATCH -p short                              # Partition to run in
+#SBATCH --mem=2000                            # Memory total in MB (in all cores)
+#SBATCH -e /ERROR-LOCATION/hostname_%j.err    # File to which STDERR will be written, including jobID             # Change/Delete this
+#SBATCH --mail-type=ALL                       # Type of email notification- BEGIN, END, FAIL, ALL
+#SBATCH --mail-user=EMAIL@host.com            # Email where notifications send to                                 # Change/Delete this
 
 # Loading required modules
 module load gcc/6.2.0
@@ -22,18 +22,15 @@ tabix -p vcf $2
 
 # 2. Merge genotype and phenotype files by "annotating" ClinVar  with the variables from gnomAD that want. Output is VCF file.
 
-bcftools annotate -a $1  -c CHROM,POS,REF,ALT,INFO/AC,INFO/AN,INFO/AF,INFO/nhomalt,INFO/AC_popmax,INFO/AN_popmax,INFO/AF_popmax,INFO/AC_afr,INFO/AN_afr,INFO/AF_afr $2  > temp_merge_1.vcf
-
+bcftools annotate -a $1 -c CHROM,POS,REF,ALT,INFO/AC,INFO/AN,INFO/AF,INFO/nhomalt,INFO/AC_popmax,INFO/AN_popmax,INFO/AF_popmax,INFO/AC_afr,INFO/AN_afr,INFO/AF_afr $2 > temp_merge_1.vcf
 
 # 3. Query variables that you want. Add additional variables here. Output is TSV file.
 
-bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%INFO/ALLELEID\t%INFO/AC\t%INFO/AN\t%INFO/AF\t%INFO/nhomalt\t%INFO/CLNDN\t%INFO/CLNREVSTAT\t%INFO/CLNSIG\t%INFO/CLNVC\t%INFO/GENEINFO\t%INFO/ORIGIN\n' temp_merg$
-
+bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%INFO/ALLELEID\t%INFO/AC\t%INFO/AN\t%INFO/AF\t%INFO/nhomalt\t%INFO/CLNDN\t%INFO/CLNREVSTAT\t%INFO/CLNSIG\t%INFO/CLNVC\t%INFO/GENEINFO\t%INFO/ORIGIN\n' temp_merge_1.vcf > temp_merge_2.tsv
 
 # 4. Add header in. Make sure column names align with the order that they were queried. Output is TSV file.
 
 echo -e "CHROM\tPOS\tREF\tALT\tALLELEID\tAC\tAN\tAF\tnhomalt\tCLNDN\tCLNREVSTAT\tCLNSIG\tCLNVC\tGENEINFO\tORIGIN\n" | cat - temp_merge_2.tsv > temp_merge_3.tsv
-
 
 # 5.A Filter file to:
                 #a. Exclude variants that are missing genotype info (chose AC to filter here)
@@ -49,11 +46,11 @@ awk 'NR==1 || !($6 == ".") && !($6 == "0") && (($11=="criteria_provided,_multipl
                                 #c. germline,somatic,maternal=1+2+16=19
 
 #awk 'NR==1 || (($15==1) || ($15==3) || ($15==16))' temp_merge_4.tsv > temp_merge_4.tsv
+
 # 6. Generate the final output file.
         #a. Include gnomAD database type (exome vs genome) in final output filename
         #b. Filter by GENEINFO provided in command line. Include GENEINFO name in final outuput filename
         #c. Filter by CLNDN (clinical disease name) if provided in command line. Include CLNDN name in final output filename
-
 
 if [[ $1  =~ ^gnomad.exome ]]; then
 
