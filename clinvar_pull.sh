bd0cd7e875c924163b56b2fc1d70bbfa8f1fd46e
preempt-merge-tool <92991232+preempt-merge-tool@users.noreply.github.com>
2021-10-22 09:41:16 -0400

    Took out identifying information

--- clinvar_pull.sh
+++ clinvar_pull.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-#SBATCH -c 1                    # Request one core
-#SBATCH -N 1                    # Request one node
+#SBATCH -c 1                                  # Request one core
+#SBATCH -N 1                                  # Request one node
 
-#SBATCH -t 0-12:00              # Run time D-HH:MM format
-#SBATCH -p short                # Partition to run in
-#SBATCH --mem=2000              # Memory total in MB (in all cores)
-#SBATCH -e /home/sks59/merge-tool/clinvar-master/error/hostname_%j.err  # File to which STDERR will be written, including jobID         # Change/Delete this
-#SBATCH --mail-type=ALL         # Type of email notification- BEGIN, END, FAIL, ALL
-#SBATCH --mail-user=skstein2@gmail.com  # Email where notifications send to                                 # Change/Delete this
+#SBATCH -t 0-12:00                            # Run time D-HH:MM format
+#SBATCH -p short                              # Partition to run in
+#SBATCH --mem=2000                            # Memory total in MB (in all cores)
+#SBATCH -e /ERROR-LOCATION/hostname_%j.err    # File to which STDERR will be written, including jobID             # Change/Delete this
+#SBATCH --mail-type=ALL                       # Type of email notification- BEGIN, END, FAIL, ALL
+#SBATCH --mail-user=EMAIL@host.com            # Email where notifications send to                                 # Change/Delete this
 
 # Loading required module
-#cd /home/gao4/clinvar-master/src
 module load gcc/6.2.0
 module load python
 
-
 # Gnomad pull and reformat
-# 1. Run pull script. This pulls both clinvar and gnomad
+# Run pull script. This pulls both clinvar and gnomad
 python clinvar_pull.py
 
 # Add in date
